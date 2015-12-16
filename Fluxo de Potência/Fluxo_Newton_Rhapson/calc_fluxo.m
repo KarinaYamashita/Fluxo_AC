6a11a008ef80f87d05669b7a24167eb36037c870
David Krepsky <david@akenge.com>
2015-12-16 13:03:08 -0200

    ajustes

--- Fluxo de Potência/Fluxo_Newton_Rhapson/calc_fluxo.m
+++ Fluxo de Potência/Fluxo_Newton_Rhapson/calc_fluxo.m
@@ -1,5 +1,5 @@
 function calc_fluxo(G,B,V,theta,Y_Sh,N_i,N_f,Sb)
-    fprintf('De  Para     Pkm       Pmk       Qkm       Qmk           Perdas\n')
+%     fprintf('De  Para     Pkm       Pmk       Qkm       Qmk           Perdas\n')
     for i = 1:length(N_i)
         k = N_i(i);
         m = N_f(i);
@@ -11,6 +11,6 @@ function calc_fluxo(G,B,V,theta,Y_Sh,N_i,N_f,Sb)
         %Pt = Pt + Pkm + Pmk;
         %Qt = Qt + Qkm + Qmk;
         %fprintf('%2d   %2d    %+4.3f    %+4.3f    %+4.3f    %+4.3f     %+4.3f    %+4.3f \n',a(i),B(i),Pkm,Qkm,Pmk,Qmk,Pkm+Pmk,Qkm+Qmk)
-        fprintf('%2d & %2d & %+4.3f & %+4.3f & %+4.3f & %+4.3f & %+4.3f & %+4.3f  \n',N_i(i),N_f(i),Pkm(k)*Sb,Pmk(k)*Sb,Qkm(k)*Sb,Qmk(k)*Sb,(Pkm(k)+Pmk(k))*Sb,(Qkm(k)+Qmk(k))*Sb)
+%         fprintf('%2d & %2d & %+4.3f & %+4.3f & %+4.3f & %+4.3f & %+4.3f & %+4.3f  \n',N_i(i),N_f(i),Pkm(k)*Sb,Pmk(k)*Sb,Qkm(k)*Sb,Qmk(k)*Sb,(Pkm(k)+Pmk(k))*Sb,(Qkm(k)+Qmk(k))*Sb)
     end
 end
